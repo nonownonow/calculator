e9f873b4a6398375f30466ab91bec03e9cc5310e
ryuilhwan <ih.ryu.dev@gmail.com>
2021-11-14 22:40:47 +0900

    implement the caculator with test

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,7 +1,25 @@
 import { expect } from 'chai/index.mjs'
+import { add, subtract, multiply, divide } from '../src/index.js'
 
 describe('calculator', () => {
-  it('add', () => {
-    expect(true).to.equal(true)
+  describe('add', () => {
+    it('add', () => {
+      expect(add(9, 3)).to.equal(12)
+    })
+  })
+  describe('substract', () => {
+    it('substract', () => {
+      expect(subtract(9, 3)).to.equal(6)
+    })
+  })
+  describe('multiply', () => {
+    it('multiply', () => {
+      expect(multiply(9, 3)).to.equal(27)
+    })
+  })
+  describe('divide', () => {
+    it('divide', () => {
+      expect(divide(9, 3)).to.equal(3)
+    })
   })
 })
